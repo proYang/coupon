408a2a4610cf72f17b9e5b56179b72a619b45cf7
SlaneYang <904509221@qq.com>
2017-05-21 03:54:02 +0800

    :construction_worker:新增webpack build过程自动部署CDN功能

---
+++ config/deploy.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  NODE_ENV: 'deploy',
+  assetsPublicPath: 'http://oov4llx6e.bkt.clouddn.com/assets/',
+  config: {
+    ACCESS_KEY: '',
+    SECRET_KEY: '',
+    bucket: 'mark-editor',
+    path: 'assets/'
+  }
+}
