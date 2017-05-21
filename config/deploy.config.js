396b1e004c58fc1eef2f5e7a340399a38977d075
SlaneYang <904509221@qq.com>
2017-05-21 11:43:18 +0800

    :wrench:更改cdn所在域名

--- config/deploy.config.js
+++ config/deploy.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: 'deploy',
-  assetsPublicPath: 'http://oqa8m4ojp.bkt.clouddn.com/assets/',
+  assetsPublicPath: 'http://cdn.coupon.crazyc.cn/assets/',
   config: {
     ACCESS_KEY: '',
     SECRET_KEY: '',
